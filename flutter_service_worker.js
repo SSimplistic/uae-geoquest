4d66b4bf37f6ae0c1c07c1c88f32452fd10e685f
imran <simplistic0420@gmail.com>
2026-02-22 22:25:49 +0400

    Update Flutter PWA

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0128f2e84c7360dc3a9f52f1effc8458",
+const RESOURCES = {"flutter_bootstrap.js": "c337a69e991ecb1a9325573fb3a3b191",
 "version.json": "1127c2eb8feddf9cd3d4512dc7f9b608",
 "index.html": "34346d0dcd8340c18ab385663f4177c3",
 "/": "34346d0dcd8340c18ab385663f4177c3",
